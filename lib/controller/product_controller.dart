6456cbb54ec3da79b9da32aee473e9618d2b4668
Luqmanasif1 <muhummadluqman67@gmail.com>
2023-01-16 00:26:31 +0500

    Solve bug

--- lib/controller/product_controller.dart
+++ lib/controller/product_controller.dart
@@ -4,25 +4,23 @@ import 'package:test_app/model/product_model.dart';
 
 class ProductController extends GetxController {
   final allProducts = <Product>[].obs;
-  List<Product> filteredProduct = [];
+  final filteredProduct = <Product>[].obs;
   @override
   void onInit() {
     allProducts.bindStream(FirestoreDB().getAllProducts());
-    filteredProduct = allProducts;
-
+    filteredProduct.value = allProducts;
     super.onInit();
   }
 
   void searchProduct(String pname) {
     if (pname.isEmpty) {
-      filteredProduct = allProducts;
+      filteredProduct.value = allProducts;
     } else {
       List<Product> results = allProducts
           .where((element) =>
               element.name.toLowerCase().contains(pname.toLowerCase()))
           .toList();
-      filteredProduct = results;
+      filteredProduct.value = results;
     }
-    update();
   }
 }
--- lib/screen/product_catalogue.dart
+++ lib/screen/product_catalogue.dart
@@ -8,45 +8,45 @@ import '../controller/cart_controller.dart';
 
 class ProductCatalogue extends StatelessWidget {
   ProductCatalogue({super.key});
+
   final cartController = Get.put(CartController());
   final productController = Get.put(ProductController());
+
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<ProductController>(
-        init: ProductController(),
-        builder: (logic) {
-          return Expanded(
-            child: AlignedGridView.count(
-              crossAxisCount: 3,
-              mainAxisSpacing: 2,
-              crossAxisSpacing: 2,
-              itemCount: productController.filteredProduct.length,
-              itemBuilder: (context, index) {
-                Product product = productController.filteredProduct[index];
-                return Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.network(product.imageUrl),
-                      Text(product.name),
-                      TextButton(
-                        onPressed: () {
-                          cartController.addProduct(product);
-                        },
-                        style: TextButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          textStyle: const TextStyle(
-                            fontSize: 16,
-                          ),
-                          backgroundColor: Colors.brown[200],
-                        ),
-                        child: const Text("Add to Cart"),
-                      )
-                    ],
-                  ),
-                );
-              },
-            ),
-          );
-        });
+    return Obx(
+      () => Expanded(
+        child: AlignedGridView.count(
+          crossAxisCount: 3,
+          mainAxisSpacing: 2,
+          crossAxisSpacing: 2,
+          itemCount: productController.filteredProduct.length,
+          itemBuilder: (context, index) {
+            Product product = productController.filteredProduct[index];
+            return Card(
+              child: Column(
+                children: <Widget>[
+                  Image.network(product.imageUrl),
+                  Text(product.name),
+                  TextButton(
+                    onPressed: () {
+                      cartController.addProduct(product);
+                    },
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      textStyle: const TextStyle(
+                        fontSize: 16,
+                      ),
+                      backgroundColor: Colors.brown[200],
+                    ),
+                    child: const Text("Add to Cart"),
+                  )
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
